338d8c0acbdac3e1f90f46cba28f5fc308aceb5d
christiansiegel <christiansiegel@users.noreply.github.com>
2016-12-14 11:36:50 +0100

    fixed classpath for javah

--- cmake/javah.sh
+++ cmake/javah.sh
@@ -7,5 +7,5 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 API_DIR=$DIR/../src/main/jniLibs
 STUB_DIR=$DIR/../src/test/jniLibs
 
-javah -jni -classpath $DIR/src/main/java/ -d $API_DIR com.nabto.api.NabtoCApiWrapper || exit 1
-javah -jni -classpath $DIR/src/test/java/ -d $STUB_DIR com.nabto.api.NabtoCApiWrapperStubController || exit 1
+javah -jni -classpath $DIR/../src/main/java/ -d $API_DIR com.nabto.api.NabtoCApiWrapper || exit 1
+javah -jni -classpath $DIR/../src/test/java/ -d $STUB_DIR com.nabto.api.NabtoCApiWrapperStubController || exit 1
