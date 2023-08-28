f5f6d1616f1202e874834c40486b96c5d61d3ff9
Michael <mkm@nabto.com>
2023-08-28 13:50:54 +0200

    test Version using connectedAndroidTest

---
+++ src/androidTest/java/com/nabto/api/NabtoApiTest.java
@@ -0,0 +1,30 @@
+package com.nabto.api;
+
+import android.util.Log;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(AndroidJUnit4.class)
+public class NabtoApiTest {
+    @Test
+    public void nabtoVersion() {
+        NabtoAndroidAssetManager assetManager = new NabtoAndroidAssetManager(InstrumentationRegistry.getInstrumentation().getContext());
+        NabtoApi api = new NabtoApi(assetManager);
+
+        String v = api.version();
+
+        assertTrue(v, v.matches("^\\d+\\.\\d+.*$"));
+    }
+}
